607ad977e8eb533c5c81b6b5dd030e651c088ab8
DevPed <pedromfr91@gmail.com>
2021-09-28 19:18:13 +0200

    Handle Add to Cart Button

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Rating from '../components/Rating';
@@ -9,77 +9,107 @@ import { detailsProduct } from '../actions/productActions';
 export default function ProductScreen(props) {
   const dispatch = useDispatch();
   const productId = props.match.params.id;
+  const [qty, setQty] = useState(1);
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
 
   useEffect(() => {
     dispatch(detailsProduct(productId));
   }, [dispatch, productId]);
-
+  const addToCartHandler = () => {
+    props.history.push(`/cart/${productId}?qty=${qty}`)
+  };
   return (
+
     <div>
-      {loading ? (
-        <LoadingBox></LoadingBox>
-      ) : error ? (
-        <MessageBox variant='danger'>{error}</MessageBox>
-      ) : (
-        <div>
-          <Link to='/'>Back to result</Link>
-          <div className='row top'>
-            <div className='col-2'>
-              <img className='large' src={product.image} alt={product.name}></img>
-            </div>
-            <div className='col-1'>
-              <ul>
-                <li>
-                  <h1>{product.name}</h1>
-                </li>
-
-                <li>
-                  <Rating
-                    rating={product.rating}
-                    numReviews={product.numReviews}
-                  ></Rating>
-                </li>
-                <li>Price: ${product.price}</li>
-                <li>
-                  Description:
-                  <p>{product.description}</p>
-                </li>
-              </ul>
-            </div>
-
-            <div className='col-1'>
-              <div className='card card-body'>
+      {
+        loading ? (
+          <LoadingBox></LoadingBox >
+        ) : error ? (
+          <MessageBox variant='danger'>{error}</MessageBox>
+        ) : (
+          <div>
+            <Link to='/'>Back to result</Link>
+            <div className='row top'>
+              <div className='col-2'>
+                <img className='large' src={product.image} alt={product.name}></img>
+              </div>
+              <div className='col-1'>
                 <ul>
                   <li>
-                    <div className='row'>
-                      <div>Price</div>
-                      <div className='price'>${product.price}</div>
-                    </div>
+                    <h1>{product.name}</h1>
                   </li>
+
                   <li>
-                    <div className='row'>
-                      <div>Status</div>
-                      <div>
-                        {product.countInStock > 0 ? (
-                          <span className='succes'>In Stock</span>
-                        ) : (
-                          <span className='danger'>Unavailable</span>
-                        )}
-                      </div>
-                    </div>
+                    <Rating
+                      rating={product.rating}
+                      numReviews={product.numReviews}
+                    ></Rating>
                   </li>
+                  <li>Price: ${product.price}</li>
                   <li>
-                    <button className='primary block'>Add to Cart</button>
+                    Description:
+                    <p>{product.description}</p>
                   </li>
                 </ul>
               </div>
+
+              <div className='col-1'>
+                <div className='card card-body'>
+                  <ul>
+                    <li>
+                      <div className='row'>
+                        <div>Price</div>
+                        <div className='price'>${product.price}</div>
+                      </div>
+                    </li>
+                    <li>
+                      <div className='row'>
+                        <div>Status</div>
+                        <div>
+                          {product.countInStock > 0 ? (
+                            <span className='succes'>In Stock</span>
+                          ) : (
+                            <span className='danger'>Unavailable</span>
+                          )}
+                        </div>
+                      </div>
+                    </li>
+                    {
+                      product.countInStock > 0 && (
+                        <>
+                          <li>
+                            <div class="row">
+                              <div>Qty</div>
+                              <div>
+                                <select onChange={e => setQty(e.target.value)} value={qty}>
+                                  {
+                                    [...Array(product.countInStock).keys()].map(
+                                      (x) => (
+                                        <option key={x + 1} value={x + 1}>{x + 1}</option>
+                                      ))
+                                  }
+                                </select>
+                              </div>
+                            </div>
+                          </li>
+                          <li>
+                            <button onClick={addToCartHandler}
+                              className='primary block'>Add to Cart</button>
+                          </li>
+                        </>
+
+                      )
+                    }
+
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )
+      }
+    </div >
 
   );
 }
