3609331d50cc46132574d61255343d9317de6cd5
DevPed <pedromfr91@gmail.com>
2021-11-08 03:19:04 +0100

    Create Product in MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import data from './data.js';
+import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
+
+// eslint-disable-next-line no-undef
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/AMAZONCLONE', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not Found' });
-  }
-});
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
@@ -30,6 +20,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
 app.listen(5000, () => {
   console.log(`Serve at http://127.0.0.1:${port}`);
